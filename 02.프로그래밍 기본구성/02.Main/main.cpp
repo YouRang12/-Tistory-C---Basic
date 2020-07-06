6a20f18ccf0f729d5e440739a5342fc40e750317
YouRang <43464752+YouRang12@users.noreply.github.com>
2020-07-06 11:10:58 +0900

    YouRang
    파일 공유

---
+++ 02.프로그래밍 기본구성/02.Main/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello Nice!!!\n");
+	printf("%d\n", 234);
+	printf("%d %d %d\n", 20, 0, -10);
+	printf("One \n Two \n Three \n");
+
+	// 이것은 주석처리
+	/*
+		이것도 주석처리
+	*/
+}
+
---
+++ 03.변수와 연산자/03.DataType/Operator1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// 연산자 - 1.대입 연산자와 산술 연산자
+//int main(void)
+//{
+//	int num1 = 9, num2 = 2;
+//	printf("%d + %d = %d \n", num1, num2, num1 + num2);
+//	printf("%d - %d = %d \n", num1, num2, num1 - num2);
+//	printf("%d x %d = %d \n", num1, num2, num1 * num2);
+//	printf("%d / %d의 몫 = %d \n", num1, num2, num1 / num2);
+//	printf("%d / %d의 나머지 = %d \n", num1, num2, num1 % num2);
+//	
+//	return 0;
+//}
+
